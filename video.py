0a4712822f51df09a36218b19b99a965fb14e127
Bruce Yong Li <yonglisc@gmail.com>
2018-12-16 14:13:15 -0800

    Create video.py

---
+++ video.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+out = cv2.VideoWriter('output.avi',fourcc, 25.0, (1280,720))
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret==True:
+        # write the frame
+        out.write(frame)
+
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+# Release everything if job is finished
+cap.release()
+out.release()
+cv2.destroyAllWindows()
